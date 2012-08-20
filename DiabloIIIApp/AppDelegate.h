9ae74e599bda508dd3a9ac3350d23f402c44c6dd
veseto <ludataaa@mail.bg>
2012-08-20 10:49:23 +0300

    Initial Commit

---
+++ DiabloIIIApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DiabloIIIApp
+//
+//  Created by Vesela Popova on 20.08.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DiabloIIIApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DiabloIIIApp
+//
+//  Created by Vesela Popova on 20.08.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
