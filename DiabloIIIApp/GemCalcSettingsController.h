7940719a2a140aa4f5aeeb5127b41a6aa13af78d
veseto <ludataaa@mail.bg>
2012-08-20 13:58:26 +0300

    set content view size - gem calc settings

---
+++ DiabloIIIApp/GemCalcSettingsController.h
@@ -0,0 +1,14 @@
+//
+//  GemCalcSettingsController.h
+//  DiabloIIIApp
+//
+//  Created by Vesela Popova on 20.08.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GemCalcSettingsController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ DiabloIIIApp/GemCalcSettingsController.m
@@ -0,0 +1,45 @@
+//
+//  GemCalcSettingsController.m
+//  DiabloIIIApp
+//
+//  Created by Vesela Popova on 20.08.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import "GemCalcSettingsController.h"
+
+@interface GemCalcSettingsController ()
+
+@end
+
+@implementation GemCalcSettingsController
+@synthesize scrollView = _scrollView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	[_scrollView setContentSize:CGSizeMake(320, 670)];
+}
+
+- (void)viewDidUnload
+{
+    [self setScrollView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
