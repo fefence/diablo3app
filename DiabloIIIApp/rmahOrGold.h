9950a470e0b03ca8c040c44c506136fa4d9fa4ee
Deniz Murat <fefence@mail.bg>
2012-09-25 19:57:04 +0300

    Create summary view for profit calculator, add 4 images

---
+++ DiabloIIIApp/rmahOrGold.h
@@ -0,0 +1,13 @@
+//
+//  rmahOrGold.h
+//  DiabloIIIApp
+//
+//  Created by Murat, Deniz on 25.09.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rmahOrGold : UITableViewController
+
+@end
